5f54f9c90829c2b3d3a054a48e2c5e1e6dce8377
Jinsoo Park <vidkidz@gmail.com>
2020-07-15 00:09:13 +0900

    Updates

---
+++ component---src-pages-404-js-d67459a8530787eceb6a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(t,e,n){"use strict";n.r(e),n.d(e,"pageQuery",(function(){return u}));n("q1tI");var a=n("Bl7J"),o=n("vrFN"),c=n("qKvR");e.default=function(t){var e=t.data,n=t.location,u=e.site.siteMetadata.title;return Object(c.c)(a.a,{location:n,title:u},Object(c.c)(o.a,{title:"404: Not Found"}),Object(c.c)("h1",null,"Not Found"),Object(c.c)("p",null,"You just hit a route that doesn't exist... the sadness."))};var u="3159585216"}}]);
+//# sourceMappingURL=component---src-pages-404-js-d67459a8530787eceb6a.js.map
--- styles-076c998adbcdbd0eb3e8.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],[]]);
-//# sourceMappingURL=styles-076c998adbcdbd0eb3e8.js.map
